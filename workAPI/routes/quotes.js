7e244e4f57884ef9ca5dd2cb56909909ce1e7593
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2025-02-12 15:54:49 -0300

    Add files via upload

--- workAPI/routes/quotes.js
+++ workAPI/routes/quotes.js
@@ -1,46 +1,121 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Quote = require('../models/quote');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /quotes'
+    Quote.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
     });
 });
 
 router.post('/', (req, res, next) => {
-    const quote = {
+    const quote = new Quote({
+        _id: new mongoose.Types.ObjectId(),
         quote: req.body.quote,
         author: req.body.author
-    }
-    res.status(201).json({
-        message: 'Handling POST requests to /quotes',
-        createdQuote: quote
+    });
+    quote
+    .save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Handling POST requests to /quotes',
+            createdQuote: result
+    })})
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
     });
 });
 
 router.get('/:quoteId', (req, res, next) => {
     const id = req.params.quoteId;
-    if(id === 'special'){
-        res.status(200).json({
-           message: 'You discovered the special ID',
-           id: id
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
+    Quote.findById(id)
+    .exec()
+    .then(doc => {
+        console.log("From database", doc);
+        if(doc){
+            res.status(200).json(doc); // Respond only once
+        } else {
+            res.status(400).json({message: 'No valid entry found for provided ID'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
 });
 
 router.patch('/:quoteId', (req, res, next) => {
-   res.status(200).json({
-       message: 'Updated quote!'
-   });
+    const id = req.params.quoteId;
+    const updateOps = {};
+
+    // Ensure the request body is an array of objects with 'propName' and 'value'
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+
+    // Use updateOne() instead of update() (update() is deprecated)
+    Quote.updateOne({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+        if (result.nModified > 0) {
+            res.status(200).json({
+                message: 'Quote updated',
+                result: result
+            });
+        } else {
+            // If no document was updated (e.g., ID doesn't exist or no changes were made)
+            res.status(404).json({
+                message: 'No valid entry found for provided ID or no changes to update'
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
 
+
 router.delete('/:quoteId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted quote!'
+    const id = req.params.quoteId;
+    Quote.findByIdAndDelete(id) // Using findByIdAndDelete instead of remove
+    .exec()
+    .then(result => {
+        if (result) {
+            // If a quote is deleted, return the deleted result
+            res.status(200).json({
+                message: 'Quote deleted',
+                deletedQuote: result
+            });
+        } else {
+            // If no quote was found with that ID
+            res.status(404).json({
+                message: 'No valid entry found for provided ID'
+            });
+        }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+         error: err
+      });
     });
  });
 
