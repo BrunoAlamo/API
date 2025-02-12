7b9f2a711ecd54d1b6dc4f77d0a86ba2adf4e16a
BrunoAlamo <147200493+BrunoAlamo@users.noreply.github.com>
2025-02-12 15:54:03 -0300

    Add files via upload

--- workAPI/app.js
+++ workAPI/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const quoteRotues = require('./routes/quotes');
 const orderRotues = require('./routes/orders');
+
+mongoose.connect('mongodb+srv://sliskebaggins:zaros1415926535@cluster0.iwtwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
